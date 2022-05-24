b192d32a2649f30ca7e0a441c37d09ea59ffc32c
matheus <matheuscarvalhopro@gmail.com>
2022-05-24 20:04:44 -0300

    criaçao da tela de noticia

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import tela from "./componetes/tela de noticia/index"
+import Tela from "./componetes/tela de noticia/index"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>matheus</Text>
-      <tela></tela>
+    <View>
+      <Text>matheus carvalho</Text>
+      <Tela></Tela>
      
     </View>
   );
--- componetes/tela de noticia/index.js
+++ componetes/tela de noticia/index.js
@@ -1,31 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import noticia from "../../Assests/logo.png"
+import noticia from "../../Assests/imagemdenoticia.webp"
+import style from './style';
+import styles from "./style"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source={noticia}/>
-      <Text>Para compartilhar esse conteúdo, por favor utilize o link https://estudio.folha.uol.com.br/bradesco-vida-e-previdencia/2022/04/a-importancia-de-sermos-protagonistas-do-nosso-futuro.shtml ou as ferramentas oferecidas na página. Textos, fotos, artes e vídeos da Folha estão protegidos pela legislação brasileira sobre direito autoral. Não reproduza o conteúdo do jornal em qualquer meio de comunicação, eletrônico ou impresso, sem autorização da Folhapress (pesquisa@folhapress.com.br). As regras têm como objetivo proteger o investimento que a Folha faz na qualidade de seu jornalismo. Se precisa copiar trecho de texto da Folha para uso privado, por favor logue-se como assinante ou cadastrado.</Text>
-      <Image 
-      style={styles.image}
-      source={{
-        uri: 'https://image.winudf.com/v2/image1/Y29tLmFqaW5mby53bG9zX3NjcmVlbl8wXzE2Mjk2OTEyMTNfMDg3/screen-0.jpg?fakeurl=1&type=.jpg',
-      }}/>   
+    <View style= {styles.container}>
+      <Text style= {styles.titulo}>Site de Noticia </Text>
+      <Image source={noticia} style= {styles.Image} />
+      <Text style= {style.texto} >Para compartilhar esse conteúdo, por favor uti ou as ferramentas oferecidas na página. Textos, fotos, artes e vídeos da Folha estão protegidos pela legislação brasileira sobre direito autoral. Não reproduza o conteúdo do jornal em qualquer meio de comunicação, eletrônico ou impresso, sem autorização da Folhapress . As regras têm como objetivo proteger o investimento que a Folha faz na qualidade de seu jornalismo. Se precisa copiar trecho de texto da Folha para uso privado, por favor logue-se como assinante ou cadastrado.</Text>
+     
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image:{
-    width: 100,
-    height:50,
-  }
-});
+
