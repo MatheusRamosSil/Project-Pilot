172e8273856595096d30742e61bb4abe30b33bd8
matheus <matheuscarvalhopro@gmail.com>
2022-05-24 17:38:17 -0300

    primeira noticia

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import tela from "./componetes/tela de noticia/index"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js</Text>
-      <Image 
-      style={styles.image}
-      source={{
-        uri: 'https://image.winudf.com/v2/image1/Y29tLmFqaW5mby53bG9zX3NjcmVlbl8wXzE2Mjk2OTEyMTNfMDg3/screen-0.jpg?fakeurl=1&type=.jpg',
-      }}/>   
+      <Text>matheus</Text>
+      <tela></tela>
+     
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image:{
-    width: 100,
-    height:50,
-  }
-});
