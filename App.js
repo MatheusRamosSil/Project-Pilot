efd09f10f6d1fd6a7cc66803a738107dcc727e50
unknown <contato.nevespedro@gmail.com>
2022-05-24 13:44:27 -0300

    tela home concluida

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './Components/Screens/Rotas/routes'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js</Text>
-      <Image 
-      style={styles.image}
-      source={{
-        uri: 'https://image.winudf.com/v2/image1/Y29tLmFqaW5mby53bG9zX3NjcmVlbl8wXzE2Mjk2OTEyMTNfMDg3/screen-0.jpg?fakeurl=1&type=.jpg',
-      }}/>   
-    </View>
+    <NavigationContainer>
+    <Routes/>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image:{
-    width: 100,
-    height:50,
-  }
-});
---
+++ Components/Screens/TelaVideos/Index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+      <View style = {styles.container}>
+          <Text styles = {styles.title}> essa e a tela de videos</Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      title:{
+        fontSize:38,
+        fontFamily:'serif',
+        textAlign:'left',
+        textTransform:'uppercase',
+      },
+   
+});
