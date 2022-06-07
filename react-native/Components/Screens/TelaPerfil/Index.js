e61705e16db0fe71ae241951e781e2f2708c8eb5
matheus <matheuscarvalhopro@gmail.com>
2022-06-07 19:02:24 -0300

    tela de profile

--- react-native/Components/Screens/TelaPerfil/Index.js
+++ react-native/Components/Screens/TelaPerfil/Index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import imagem from '../../../Assests/david.png'
+
 
 export default function App() {
   return (
-      <View style = {styles.container}>
-          <Text styles = {styles.title}> essa e a tela de perfil</Text>
+
+
+    
+      <View style={styles.container}>
+          <View style={styles.novaview}>
+                <Image style={styles.imagem} source={imagem}/> 
+                <Text style={styles.texto}>David Lopez</Text>
+                <Text style={styles.texto}>DavidLopezgp@unipe.com.br</Text>
+          </View>
+          <View style={styles.viewbotao}>
+                <Pressable style={styles.botao}><Text style={styles.david}>profile</Text></Pressable>
+                <Pressable style={styles.botao}><Text style={styles.david}>settings</Text></Pressable>
+                <Pressable style={styles.botao}><Text style={styles.david}>logout</Text></Pressable>
+                <Pressable style={styles.botao}><Text style={styles.david}>support</Text></Pressable>
+          </View>      
       </View>
+
   );
 }
 
@@ -22,5 +38,39 @@ const styles = StyleSheet.create({
         textAlign:'left',
         textTransform:'uppercase',
       },
+      botao:{
+        width:300,
+        height:50,
+        backgroundColor:'#F68989',
+        padding:10,
+        margin:5,
+        borderRadius:80,
+        marginTop:5,
+      },
+      imagem:{
+        width:100,
+        height:100,
+        borderRadius:100,
+        marginBottom:40,
+        alignItems:'center'
+      },
+      david:{
+        textAlign:'center',
+        backgroundColor:'#FFAB76',
+      },
+      novaview:{
+        alignItems:'center',
+        textAlign:'center',
+        justifyContent:'start',
+        marginBottom:80,
+      },
+      viewbotao:{
+        marginTop:undefined,
+
+
+
+      }
+
+
    
 });
